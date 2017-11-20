078a20e7bb6bc92f5f6f897e821537a99a971ba5
musle <musle@gscc.net.tw>
2017-11-20 17:27:44 +0800

    test 改變9秒改變一次 dc1500 狀態

--- slave/unslave_eos-c3/1.cmd
+++ slave/unslave_eos-c3/1.cmd
@@ -1,9 +1,9 @@
 @ REM -- FOR DC1500 768 狀態
 :loop
 @echo loop1--
-call newman run cl3.json --delay-request 7000
+call newman run cl3.json --delay-request 9000
 @echo loop2--
-call newman run cl3.json --delay-request 7000
+call newman run cl3.json --delay-request 9000
 @echo loop3--
-call newman run cl3.json --delay-request 7000
+call newman run cl3.json --delay-request 9000
 goto :loop
